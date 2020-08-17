5a86760bfddb92e8343119fc47c28c36d1da4438
janson <janson_j_shan@163.com>
2020-08-17 23:26:45 +0800

    提交Java性能调优实践项目初始化

---
+++ src/main/java/com/janson/performance/optimization/PerformanceOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.janson.performance.optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceOptimizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/janson/performance/optimization/PerformanceOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janson.performance.optimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
