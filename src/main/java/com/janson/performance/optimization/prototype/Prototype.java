0a0429ecf31a53339c6f4ab0ea88b9df6561a3ba
janson <janson_j_shan@163.com>
2020-11-30 11:06:47 +0800

    提交原型模式

---
+++ src/main/java/com/janson/performance/optimization/prototype/Prototype.java
@@ -0,0 +1,11 @@
+package com.janson.performance.optimization.prototype;
+
+/**
+ * @Description: 原型类
+ * @Author: Janson
+ * @Date: 2020/11/30 10:09
+ **/
+public class Prototype implements Cloneable {
+
+
+}
